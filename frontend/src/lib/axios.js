5cecffb2e423f895f9d194014baa39e62afc308a
rajeshrys <rajeshthati535@gmail.com>
2026-05-26 21:02:22 +0530

    Change API base URL to production server

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://ai-resume-analyzer-gfa4.onrender.com/api",
     withCredentials:true
 })
 
